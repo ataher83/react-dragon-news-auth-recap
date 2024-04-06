7f0ba3e8a0d1279acf1d5b06b3e543600f01f2a7
ataher83 <taher83@gmail.com>
2024-04-06 09:39:28 +0600

    project upload on new repo

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 </div>
             </div>
 
-                <Link>
+                <Link to="/login">
                     <button className="btn">Login</button>
                 </Link>
             </div>
